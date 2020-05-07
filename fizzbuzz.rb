75194b2fc57fa5150e23f454734079302d3b2974
Matthew Blank <msb277@cornell.edu>
2020-05-07 17:24:41 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(x)
+  remainder_5 = x % 5
+  remainder_3 = x % 3
+  if  == 0 && x % 5 == 0
+
 
 end
